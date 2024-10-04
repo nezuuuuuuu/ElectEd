825d6d2ea3e0b89aea0fd154fc0a0959b9fb0f80
dzanloi <carreon.johnloi@gmail.com>
2024-10-04 10:48:39 +0800

    Added Dashboard nga template pa

--- elected/urls.py
+++ elected/urls.py
@@ -20,5 +20,8 @@ from django.urls import path, include
 urlpatterns = [
     path("__reload__/", include("django_browser_reload.urls")),
     path('admin/', admin.site.urls),
-    path('', include("landing_page.urls"))
+    path('', include("landing_page.urls")),
+
+    # DASHBOARD
+    path('dashboard/', include("dashboard.urls")),
 ]   
