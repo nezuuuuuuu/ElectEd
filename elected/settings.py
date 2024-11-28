6019f59df63428e2eeb4956ed9ff3687243e4fec
nezu <janednward.abadiano@cit.edu>
2024-11-28 23:51:29 +0800

    fixed upcoming timer

--- elected/settings.py
+++ elected/settings.py
@@ -122,8 +122,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Manila'
+# USE_TZ = True
+# TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
